5c5fc6171f0a29641553b12780cf98f43c7d177c
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2025-05-08 23:05:23 +0900

    floating button scroll fn added

--- src/components/common/BaseIconButton.tsx
+++ src/components/common/BaseIconButton.tsx
@@ -1,23 +1,27 @@
-
 import { ReactNode } from "react";
 
 interface BaseIconButtonProps {
   onClick?: () => void;
-  href?: string; 
+  href?: string;
   children: ReactNode;
   title?: string;
 }
 
-const BaseIconButton = ({ onClick, href, children, title }: BaseIconButtonProps) => {
+const BaseIconButton = ({
+  onClick,
+  href,
+  children,
+  title,
+}: BaseIconButtonProps) => {
   const commonClass =
-    "w-12 h-12 rounded-full flex items-center justify-center shadow-xl bg-white border-4 border-gray-700";
+    "w-12 h-12 rounded-full flex items-center justify-center shadow-xl bg-white border-4 border-gray-700 hover:scale-105";
 
   if (href) {
     return (
       <a
         href={href}
         target="_blank"
-        rel=""
+        rel="noopener noreferrer"
         className={commonClass}
         title={title}
       >
--- src/pages/FloatingButton.tsx
+++ src/pages/FloatingButton.tsx
@@ -3,89 +3,32 @@ import { FloatingData, FloatingIcon } from "../data/FloatingData";
 import React from "react";
 
 type IconProps = {
-    size?: number;
-    className?: string;
+  size?: number;
+  className?: string;
 };
 
 const FloatingButton = () => {
-    return (
-        <div className="fixed bottom-4 right-4 flex flex-col z-50 gap-4">
-            {FloatingData.map(({ key, value, name }) => {
-                const Icon = FloatingIcon[key] as React.ComponentType<IconProps>;
+  return (
+    <div className="fixed bottom-8 right-4 flex flex-col z-50 gap-3">
+      {FloatingData.map(({ key, value, name, type }) => {
+        const Icon = FloatingIcon[key] as React.ComponentType<IconProps>;
 
-                const handleClick = () => {
-                    navigator.clipboard.writeText(value);
-                }
+        const props = {
+          key,
+          title: name,
+          ...(type === "scroll"
+            ? { onClick: () => window.scrollTo({ top: 0, behavior: "smooth" }) }
+            : { href: `https://${value}` }),
+        };
 
-                return (
-                    <BaseIconButton key={key} onClick={handleClick} href={`https://${value}`} title={name}>
-                        <Icon size={24} className="hover:bg-gray-200" />
-                    </BaseIconButton>
-                )
-            })}
-        </div>
-    )
-}
+        return (
+          <BaseIconButton {...props}>
+            <Icon size={24} />
+          </BaseIconButton>
+        );
+      })}
+    </div>
+  );
+};
 
 export default FloatingButton;
-
-
-
-
-
-
-
-
-
-// import BaseIconButton from "../components/common/BaseIconButton";
-// import { FloatingData, FloatingIcon } from "../data/FloatingData";
-// import React from "react";
-
-// type IconProps = {
-//     size?: number;
-//     className?: string;
-// };
-
-// const FloatingButton = () => {
-//     return (
-//         <div className="fixed bottom-4 right-4 flex flex-col z-50 gap-4">
-//             {FloatingData.map(({ key, value, name, type }) => {
-//                 const Icon = FloatingIcon[key] as React.ComponentType<IconProps>;
-//                 let content: React.ReactNode;
-
-//                 const handleClick = () => {
-//                     if (type === "url") {
-//                         content = (
-//                             <a
-//                                 href={`https://${value}`}
-//                                 target="_blank"
-//                                 rel="noopener noreferrer"
-//                                 className="hover:underline"
-//                             >
-//                                 {value}
-//                             </a>
-//                         )
-//                     } else {
-//                         content = (
-//                             <button onClick={handleClick} className="hover:underline">
-//                                 {value}
-//                             </button>
-//                         );
-//                     }
-//                 }
-
-
-
-//                 // if (type === "scroll")
-
-//                 return (
-//                     <BaseIconButton key={key} title={name}>
-//                         <Icon size={24} />
-//                     </BaseIconButton>
-//                 )
-//             })}
-//         </div>
-//     )
-// }
-
-// export default FloatingButton;
