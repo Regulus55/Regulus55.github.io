990b7a87fac3473f706f2a769287a9e41c0365b8
Regulus55 <hakjoon55@gmail.com>
2025-05-08 17:48:22 +0900

    floating test

--- src/data/FloatingData.tsx
+++ src/data/FloatingData.tsx
@@ -4,12 +4,12 @@ import { SiVelog } from "react-icons/si";
 
 export const FloatingIcon: Record<string, IconType> = {
     github: FaGithub,
-    blog: SiVelog ,
+    blog: SiVelog,
     top: FaChevronUp,
 };
 
 export const FloatingData = [
     { key: "blog", name: "블로그", value: "velog.io/@regulus", type: "url" },
     { key: "github", name: "깃허브", value: "github.com/Regulus55", type: "url" },
-    { key: "top", name: "맨위로", value: "regulus55.github.io", type: "top" },
+    { key: "top", name: "맨위로", value: "regulus55.github.io", type: "scroll" },
 ]
--- src/pages/FloatingButton.tsx
+++ src/pages/FloatingButton.tsx
@@ -28,3 +28,64 @@ const FloatingButton = () => {
 }
 
 export default FloatingButton;
+
+
+
+
+
+
+
+
+
+// import BaseIconButton from "../components/common/BaseIconButton";
+// import { FloatingData, FloatingIcon } from "../data/FloatingData";
+// import React from "react";
+
+// type IconProps = {
+//     size?: number;
+//     className?: string;
+// };
+
+// const FloatingButton = () => {
+//     return (
+//         <div className="fixed bottom-4 right-4 flex flex-col z-50 gap-4">
+//             {FloatingData.map(({ key, value, name, type }) => {
+//                 const Icon = FloatingIcon[key] as React.ComponentType<IconProps>;
+//                 let content: React.ReactNode;
+
+//                 const handleClick = () => {
+//                     if (type === "url") {
+//                         content = (
+//                             <a
+//                                 href={`https://${value}`}
+//                                 target="_blank"
+//                                 rel="noopener noreferrer"
+//                                 className="hover:underline"
+//                             >
+//                                 {value}
+//                             </a>
+//                         )
+//                     } else {
+//                         content = (
+//                             <button onClick={handleClick} className="hover:underline">
+//                                 {value}
+//                             </button>
+//                         );
+//                     }
+//                 }
+
+
+
+//                 // if (type === "scroll")
+
+//                 return (
+//                     <BaseIconButton key={key} title={name}>
+//                         <Icon size={24} />
+//                     </BaseIconButton>
+//                 )
+//             })}
+//         </div>
+//     )
+// }
+
+// export default FloatingButton;
