d5dd8a79b83bebdee9b3077133555b054fcf0aa8
Regulus55 <hakjoon55@gmail.com>
2025-05-12 11:35:20 +0900

    detail pages

--- src/data/projectDetail/index.tsx
+++ src/data/projectDetail/index.tsx
@@ -1,7 +1,12 @@
 import { PortfolioDetail } from "./PortfolioDetail";
 import { NarakDetail } from "./NarakDetail";
 import { GushopDetail } from "./GushopDetail";
+import { ProjectDetail } from "../../utils/types";
 
-const AllProjectDetail = [PortfolioDetail, NarakDetail, GushopDetail];
+
+const AllProjectDetail: ProjectDetail[] = [PortfolioDetail, NarakDetail, GushopDetail];
 
 export default AllProjectDetail;
+
+
+
--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useThemeStore } from "../store/useThemeStore";
 import Glassmorphism from "../components/wrapper/Glassmorphism";
 import AllProjectDetail from "../data/projectDetail";
-import { SkillsData } from "../data";
+import { Overview, Stack } from "../utils/types";
 
 const ProjectDetail = () => {
   const { slug } = useParams<{ slug: string }>();
@@ -29,7 +29,7 @@ const ProjectDetail = () => {
   if (!detail) return null;
 
   return (
-    <div className="relative flex justify-center items-center w-full min-h-screen overflow-x-hidden py-20">
+    <div className="relative text-white flex justify-center items-center w-full min-h-screen overflow-x-hidden py-20">
       <div
         className={`fixed inset-0 w-full h-full bg-cover bg-center z-0 
           ${isDayMode ? "bg-day-sky" : "bg-orange-sky"}
@@ -38,45 +38,71 @@ const ProjectDetail = () => {
       />
 
       <div className="relative max-w-7xl w-full min-h-screen z-10">
-        <Glassmorphism className="flex items-center justify-center w-full rounded-xl px-6 z-20">
+        <div className="flex items-center justify-center w-full rounded-xl px-6 z-20 bg-black/50">
           <div
             onClick={(e) => e.stopPropagation()}
-            className="flex flex-col items-center justify-center max-w-2xl w-full p-8"
+            className="flex flex-col items-center justify-center w-full p-8"
           >
-            <h1 className="text-4xl text-white font-bold">{detail.title}</h1>
-            <p className="text-md mb-4 text-gray-300">{detail.period}</p>
-            <div className="max-w-xl w-full max-h-96 overflow-hidden rounded-md mb-8">
-              <img src={detail.image} alt={detail.title} className="w-full" />
+            <div id="title" className="relative flex flex-col items-center justify-center max-w-3xl w-full mb-12">
+              <h1 className="text-4xl text-white font-bold">{detail.title}</h1>
+              <p className="text-md mb-4 text-gray-300">{detail.period}</p>
+              <div className="max-w-xl w-full h-auto overflow-hidden rounded-md mb-4">
+                <img src={detail.image} alt={detail.title} className="w-full h-full" />
+              </div>
+              <h3 className="text-2xl font-semibold">{detail.subtitle}</h3>
             </div>
-            <h3 className="text-xl font-semibold mb-4">{detail.subtitle}</h3>
-            {/* <div className="flex gap-4 bg-white p-2 rounded-lg shadow-xl">
-              {detail.skills.map((name) => {
-                const skill = SkillsData.find((s) => s.name === name);
 
-                return skill ? (
-                  <img
-                    key={name}
-                    src={skill.src}
-                    alt={skill.alt}
-                    className="h-10 w-10"
-                  />
-                ) : null;
-              })}
-            </div> */}
-
-            <div className="bg-white text-3xl font-3xl p-2">
-              {detail.skills.map((name, index) => {
-                // const skill = SkillsData.find((s) => s.name === name);
-
-                return <div key={index}>:ddddddddddddd</div>;
-              })}
+            <div id="stacks" className="relative flex flex-col items-start justify-center w-full max-w-xl">
+              <h2 className="text-xl">Stack</h2>
+              <div className="text-xl font-3xl p-2">
+                {Array.isArray(detail?.stacks) &&
+                  detail.stacks.map((skill: Stack, index: number) => {
+                    if (
+                      typeof skill === "object" &&
+                      "title" in skill &&
+                      "stack" in skill &&
+                      Array.isArray(skill.stack)
+                    ) {
+                      return (
+                        <div key={index}>
+                          <strong>{skill.title}</strong>: {skill.stack.join(", ")}
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
+              </div>
             </div>
-            <div className="flex flex-col items-center justify-center w-full border-t-2 border-gray-500 mt-10">
-              <div>dd</div>
-              <div>dd</div>
+
+            <div id="overview" className="relative flex flex-col items-start justify-center max-w-5xl w-full py-4 mt-10 border-t-2 border-gray-500">
+              <h2 className="text-xl">Overview</h2>
+              <div className="flex flex-col text-xl p-2 gap-4">
+                {Array.isArray(detail?.overview) &&
+                  detail?.overview?.map((content: Overview, index: number) => {
+                    return (
+                      <div key={index} className="">
+                        <span>{index}</span> <strong>{content.title}</strong>: {content.content}
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+
+            <div id="preview" className="relative flex flex-col items-start justify-center max-w-5xl w-full py-4 mt-10 border-t-2 border-gray-500">
+              <h2 className="text-xl">Preview</h2>
+              <div className="flex flex-col text-xl p-2 gap-4">
+                {Array.isArray(detail?.overview) &&
+                  detail?.overview?.map((content: Overview, index: number) => {
+                    return (
+                      <div key={index} className="">
+                        <span>{index}</span> <strong>{content.title}</strong>: {content.content}
+                      </div>
+                    );
+                  })}
+              </div>
             </div>
           </div>
-        </Glassmorphism>
+        </div>
       </div>
     </div>
   );
