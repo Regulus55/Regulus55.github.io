ca790f347c53e643e791d6f3bea8276d0c1580c1
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2025-05-12 23:24:11 +0900

    portfolio preview data

--- src/data/projectDetail/PortfolioDetail.tsx
+++ src/data/projectDetail/PortfolioDetail.tsx
@@ -9,11 +9,11 @@ export const PortfolioDetail: ProjectDetail = {
   overview: [
     {
       title: "프로젝트 기획 이유",
-      content: ["나만의 사이트를 만들어 나의 프로젝트와 기술을 보여주기 위함"]
+      content: ["나만의 사이트를 만들어 나의 프로젝트와 기술을 보여주기 위함"],
     },
     {
       title: "프로젝트 기간",
-      content: ["2024.11.30 ~ 현재 진행중"]
+      content: ["2024.11.30 ~ 현재 진행중"],
     },
     {
       title: "개발스택",
@@ -21,22 +21,22 @@ export const PortfolioDetail: ProjectDetail = {
         "프론트엔드 : React, JavaScript, TypeScript",
         "스타일링 : Tailwind CSS",
         "상태관리 : Zustand",
-        "라이브러리 : React Scroll Parallax, Scroll Reveal, React Toastify"
-      ]
+        "라이브러리 : React Scroll Parallax, Scroll Reveal, React Toastify",
+      ],
     },
     {
       title: "특징",
       content: [
         "스크롤을 내리면 우주에서 땅으로 내려오는 느낌이 들 수 있도록 레이아웃 제작",
         "React Scroll Parallax, ScrollReveal, Glassmorphism 등의 라이브러리를 활용해 시각적 완성도를 높임",
-        "프로젝트 카드를 클릭하면 상세 페이지로 이동 가능"
-      ]
+        "프로젝트 카드를 클릭하면 상세 페이지로 이동 가능",
+      ],
     },
     {
       title: "아쉬운 점",
       content: [
-        "초기 기획 단계에서 항성, 인공위성 등 다양한 배경 이미지를 적용하려 했지만 반영하지 못한 점"
-      ]
+        "초기 기획 단계에서 항성, 인공위성 등 다양한 배경 이미지를 적용하려 했지만 반영하지 못한 점",
+      ],
     },
     {
       title: "추후 추가할 내용",
@@ -44,22 +44,61 @@ export const PortfolioDetail: ProjectDetail = {
         "Hero Section에 들어갈 커버 이미지 제작",
         "반응형 UI 지원",
         "Footer 영역의 산과 구름 이미지에도 Parallax 효과 적용",
-        "전체적인 UI 세부 개선"
-      ]
+        "전체적인 UI 세부 개선",
+      ],
     },
     {
       title: "추후 개선할 내용",
       content: [
         "ScrollReveal이 적용된 컴포넌트가 원위치로 돌아오기 전까지 Parallax가 정상 작동하지 않는 문제",
-        "고정된 배경 이미지가 스크롤 시 밀려나는 현상"
-      ]
-    }
+        "고정된 배경 이미지가 스크롤 시 밀려나는 현상",
+      ],
+    },
   ],
   preview: [
     {
-      title: "메인페이지",
-      image: ["/images/project/narak/나락메인.png"],
-      content: ["포폴"]
-    }
-  ]
+      title: "배경이미지",
+      image: ["/images/project/portfolio/back.png"],
+      content: [
+        "우주에서 내려오는 느낌이 들도록 남색부터 하늘색까지 그라데이션으로 색을 배치했습니다",
+      ],
+    },
+    {
+      title: "메인화면",
+      image: ["/images/project/portfolio/main.png"],
+      content: ["사이트에 처음 접속했을 때 보이는 Hero Section입니다"],
+    },
+    {
+      title: "프로파일",
+      image: ["/images/project/portfolio/profile.png"],
+      content: [
+        "이름, 전화번호, 이메일 주소 클릭 시 클립보드에 내용이 복사되도록 했습니다",
+      ],
+    },
+    {
+      title: "어바웃미",
+      image: ["/images/project/portfolio/aboutme.png"],
+      content: ["나를 소개합니다"],
+    },
+    {
+      title: "프로젝트",
+      image: ["/images/project/portfolio/project.png"],
+      content: ["클릭시 프로젝트 상세 페이지로 넘어갈 수 있습니다"],
+    },
+    {
+      title: "컨택트",
+      image: ["/images/project/portfolio/contact.png"],
+      content: ["맨 아래 땅부분에 산과 구름 이미지를 배치했습니다"],
+    },
+    {
+      title: "프로젝트 상세페이지",
+      image: ["/images/project/portfolio/detail.png"],
+      content: ["상세페이지에서 Overview, Preview 이미지를 볼 수 있습니다"],
+    },
+    {
+      title: "저녁하늘배경",
+      image: ["/images/project/portfolio/sunset.png"],
+      content: ["메인화면에서 제목 클릭 시 배경색이 변하도록 하였읍니다"],
+    },
+  ],
 };
--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useThemeStore } from "../store/useThemeStore";
 import Glassmorphism from "../components/wrapper/Glassmorphism";
 import AllProjectDetail from "../data/projectDetail";
-import { Overview, Stack } from "../utils/types";
+import { Overview, Preview, Stack } from "../utils/types";
 
 const ProjectDetail = () => {
   const { slug } = useParams<{ slug: string }>();
@@ -43,23 +43,37 @@ const ProjectDetail = () => {
             onClick={(e) => e.stopPropagation()}
             className="flex flex-col items-center justify-center w-full p-8"
           >
-            <div id="title" className="relative flex flex-col items-center justify-center max-w-3xl w-full mb-8">
+            <div
+              id="title"
+              className="relative flex flex-col items-center justify-center max-w-3xl w-full mb-8"
+            >
               <h1 className="text-4xl text-white font-bold">{detail.title}</h1>
               <p className="text-md mb-4 text-gray-300">{detail.period}</p>
               <div className="max-w-xl w-full h-auto overflow-hidden rounded-md mb-6">
-                <img src={detail.image} alt={detail.title} className="w-full h-full" />
+                <img
+                  src={detail.image}
+                  alt={detail.title}
+                  className="w-full h-full"
+                />
               </div>
               <h3 className="text-2xl font-semibold">{detail.subtitle}</h3>
             </div>
 
-            <div id="overview" className="relative flex flex-col items-start justify-center max-w-4xl w-full pt-10 border-t-2 border-gray-500">
-              <h2 className="text-4xl font-bold mb-6 text-grayShadow">Overview</h2>
+            <div
+              id="overview"
+              className="relative flex flex-col items-start justify-center max-w-4xl w-full pt-10 border-t-2 border-gray-500"
+            >
+              <h2 className="text-4xl font-bold mb-6 text-greyShadow">
+                Overview
+              </h2>
               <div className="flex flex-col text-xl p-2 gap-6">
                 {Array.isArray(detail?.overview) &&
                   detail?.overview?.map((item: Overview, index: number) => {
                     return (
                       <div key={index}>
-                        <div className="text-xl font-bold mb-1">{index + 1}. {item.title}</div>
+                        <div className="text-xl font-bold mb-1">
+                          {index + 1}. {item.title}
+                        </div>
                         <ul className="text-lg">
                           {item.content?.map((element) => (
                             <li className="flex gap-3 ml-5">
@@ -74,22 +88,40 @@ const ProjectDetail = () => {
               </div>
             </div>
 
-            <div id="preview" className="relative flex flex-col items-start justify-center max-w-4xl w-full pt-10 mt-10 border-t-2 border-gray-500">
-              <h2 className="text-4xl font-bold mb-6 text-grayShadow">Preview</h2>
-              <div className="flex flex-col text-xl p-2 gap-6">
-                {Array.isArray(detail?.overview) &&
-                  detail?.overview?.map((item: Overview, index: number) => {
+            <div
+              id="preview"
+              className="relative flex flex-col items-start justify-center max-w-4xl w-full pt-10 mt-10 border-t-2 border-gray-500"
+            >
+              <h2 className="text-4xl font-bold mb-6 text-greyShadow">
+                Preview
+              </h2>
+              <div className="grid grid-cols-2 place-items-center items-start w-full text-xl">
+                {Array.isArray(detail?.preview) &&
+                  detail?.preview?.map((item: Preview, index: number) => {
                     return (
-                      <div key={index}>
-                        <div className="text-xl font-bold mb-1">{index + 1}. {item.title}</div>
-                        <ul className="text-lg">
-                          {item.content?.map((element) => (
-                            <li className="flex gap-3 ml-5">
-                              <span>●</span>
-                              <span>{element}</span>
-                            </li>
+                      <div
+                        key={index}
+                        className="flex flex-col items-center justify-center w-96 mb-8"
+                      >
+                        <div className="text-xl font-bold mb-1">
+                          {item.title}
+                        </div>
+                        <div className="mb-2">
+                          {item.image?.map((element) => (
+                            <div className="w-96 h-96 overflow-hidden">
+                              <img
+                                src={element}
+                                alt={element}
+                                className="w-full h-full object-cover"
+                              />
+                            </div>
                           ))}
-                        </ul>
+                        </div>
+                        <div className="text-lg">
+                          {item.content?.map((element) => (
+                            <div className="flex gap-3">{element}</div>
+                          ))}
+                        </div>
                       </div>
                     );
                   })}
