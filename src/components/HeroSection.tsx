81424b07e7f4c53590d1c16d3448ac872a775056
Regulus55 <hakjoon55@gmail.com>
2025-05-20 16:11:57 +0900

    hero section image changed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,19 +12,28 @@ const HeroSection = ({ scrollToSection }: HeroSectionProps) => {
   return (
     <div className="fixed top-0 left-0 w-full h-screen">
       <img
-        src="/images/background/moon.jpg"
-        // src="/images/background/moon2.png"
+        // src="/images/background/moon.jpg"
+        src="/images/background/galaxy.png"
         alt="Background"
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       />
-      <div className="relative z-10 flex items-center justify-center w-full h-full">
+
+      <div className="relative flex items-center justify-center w-full h-full z-10">
+        <div className="relative flex items-center justify-center w-full h-full z-10 p-20">
+          <img
+            src="/images/background/moonsample.png"
+            alt="moon"
+            onClick={() => scrollToSection("profile")}
+            className="w-[650px] h-[650px] mt-2 object-contain rounded-full cursor-pointer"
+          />
+        </div>
+
         <h1
-          className="text-5xl font-bold text-white mb-60 hover:cursor-pointer"
-          onClick={toggleTheme}
+          className="absolute text-5xl font-bold text-white mb-60 hover:cursor-pointer z-20"
+          onClick={() => scrollToSection("profile")}
         >
           프론트엔드 개발자 김학준 입니다
         </h1>
-        {/* <h2 onClick={() => scrollToSection("profile")}>안녕하세요</h2> */}
       </div>
     </div>
   );
