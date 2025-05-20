0f590eac7d2cfdeb88a5f21dab79df8814a4e1af
Regulus55 <hakjoon55@gmail.com>
2025-05-20 14:54:29 +0900

    background mountain image

---
+++ src/utils/scroll.ts
@@ -0,0 +1,15 @@
+// 이전페이지 기억
+export const saveScrollY = () => {
+  localStorage.setItem("scrollY", window.scrollY.toString());
+};
+
+export const restoreScrollY = () => {
+  const savedY = localStorage.getItem("scrollY");
+  if (savedY) {
+    setTimeout(() => {
+      window.scrollTo(0, parseInt(savedY, 10));
+    }, 50);
+  }
+};
+
+
