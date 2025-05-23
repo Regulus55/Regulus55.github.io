385d3df628eff67375a826117f736dd60a23fe68
Regulus55 <hakjoon55@gmail.com>
2025-05-23 11:57:37 +0900

    checkpoint

--- src/components/wrapper/ParallaxWrapper.tsx
+++ src/components/wrapper/ParallaxWrapper.tsx
@@ -6,7 +6,7 @@ const ParallaxWrapper = ({ children }: { children: React.ReactNode }) => {
   const { isDayMode } = useThemeStore();
 
   return (
-    <div className="relative min-h-screen scrollbar-none">
+    <div className="relative min-h-screen scrollbar-none overflow-hidden">
       <div className="absolute inset-0 z-5 overflow-x-hidden">
         <div
           className={`w-full min-h-full 
@@ -26,14 +26,16 @@ const ParallaxWrapper = ({ children }: { children: React.ReactNode }) => {
         </Parallax>
       </div>
 
-      <div className="absolute h-auto top-0 left-0 right-0 z-30 pointer-events-none      bg-red-700/20 border-4 border-red-500">
+      <div className="absolute h-auto top-0 left-0 right-0 z-30 pointer-events-none      ">
+        {/*   bg-red-700/20 border-4 border-red-500  */}
         <Parallax speed={-50}>
           <img
             src="/images/background/star2.png"
             alt=""
             draggable={false}
-            className="w-full h-full object-cover     bg-blue-700/20 border-4 border-blue-500"
+            className="w-full h-full object-cover     "           
           />
+           {/*   bg-blue-700/20 border-4 border-blue-500 */}
         </Parallax>
       </div>
 
