e071d790438f71ed435616b2c3e8ede0d5eb1fe9
Regulus55 <hakjoon55@gmail.com>
2025-06-09 14:24:25 +0900

    about me

--- src/pages/contents/Profile.tsx
+++ src/pages/contents/Profile.tsx
@@ -5,14 +5,22 @@ import { ReactIconProps } from "../../utils/types";
 
 const Profile = () => {
   return (
-    <div className="relative flex flex-col items-start justify-center w-full h-full max-w-7xl p-10 rounded-3xl mt-52 mb-40">
-      <ContentsTitle className="ml-6 mb-6">PROFILE</ContentsTitle>
-      <Glassmorphism className="relative flex items-center justify-start w-auto pr-60 py-10 pl-6">
-        <div className="flex items-left bg-white rounded-full ml-10 mr-20 border">
+    <div className="relative flex flex-col items-center lg:items-start justify-center w-full h-full max-w-7xl p-10 rounded-3xl mt-52 mb-40">
+      <div className="items-start">
+      <ContentsTitle className="ml-3 sm:ml-6 mb-6">PROFILE</ContentsTitle>
+      <Glassmorphism className={`relative flex flex-col  items-center justify-start w-auto px-10 py-6
+                                sm:px-20 sm:py-10
+                                 lg:flex-row lg:pl-6 lg:pr-60 lg:px-0 lg:py-10
+        `}>
+        <div className={`flex items-left bg-white rounded-full border mb-10
+                         lg:ml-10 lg:mr-20 lg:mb-0
+          `}>
           <img
             src="/images/regulus.webp"
             alt=""
-            className="h-60 w-60 rounded-full p-4"
+            className={`h-52 w-52 rounded-full p-4
+                        xl:h-60 xl:w-60
+              `}
           />
         </div>
         <div className={"flex flex-col items-left justify-center text-3xl"}>
@@ -59,9 +67,11 @@ const Profile = () => {
             return (
               <div
                 key={key}
-                className="flex items-center gap-2 mb-2 text-white"
+                className={`flex items-center  sm:gap-2 mb-3 sm:mb-2 text-white text-xl
+                            sm:text-2xl lg:text-3xl
+                  `}
               >
-                <Icon size={24} className="mr-4" />
+                <Icon className="w-4 h-4 sm:w-5 sm:h-5 lg:w-6 lg:h-6 mr-4" />
                 {content}
               </div>
             );
@@ -70,6 +80,7 @@ const Profile = () => {
 
         <div className="absolute bottom-4 right-4 w-32 h-32 rounded-full bg-white/20 blur-2xl pointer-events-none" />
       </Glassmorphism>
+    </div>
     </div>
   );
 };
